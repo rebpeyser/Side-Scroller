90597aad50e27b8d72534968fab021a91710e350
Rebecca Peyser <rebeccapeyser@rebeccas-mbp.lan>
2019-06-12 15:18:07 -0400

    Added player, background, ground, and rocks.

--- Side Scroller/GameScene.swift
+++ Side Scroller/GameScene.swift
@@ -11,79 +11,114 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var player: SKSpriteNode!
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        createPlayer()
+        createBackground()
+        createGround()
+        startRocks()
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+ 
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func createPlayer() {
+        let playerTexture = SKTexture(imageNamed: "dog-walking0")
+        player = SKSpriteNode(texture: playerTexture)
+        
+        player.zPosition = 10
+        player.position = CGPoint(x: frame.width/6, y: frame.height*0.75)
+        
+        addChild(player)
+        
+        //Setup the animation of the sprite so it looks like it's running
+        let frame2 = SKTexture(imageNamed: "dog-walking1")
+        let frame3 = SKTexture(imageNamed: "dog-walking2")
+        let animation = SKAction.animate(with: [playerTexture, frame2, frame3, frame2],   timePerFrame: 0.1)
+        let runForever = SKAction.repeatForever((animation))
+        
+        //Run the SKActions on the player sprite
+        player.run(runForever)
+        
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func createBackground() {
+        let backgroundTexture = SKTexture(imageNamed: "background")
+        for i in 0...1 {
+            let background = SKSpriteNode(texture: backgroundTexture)
+            background.zPosition = -30
+            background.anchorPoint = CGPoint(x: (backgroundTexture.size().width * CGFloat(i)) - CGFloat(1 * i), y: 0)
+            addChild(background)
+            
+            let moveLeft = SKAction.moveBy(x: -backgroundTexture.size().width, y: 0, duration: 20)
+            let moveReset = SKAction.moveBy(x: backgroundTexture.size().width, y: 0, duration: 0)
+            let moveLoop = SKAction.sequence([moveLeft, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            background.run(moveForever)
+
+        }
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func createGround(){
+        let groundTexture = SKTexture(imageNamed: "ground")
+        for i in 0...1 {
+            let ground = SKSpriteNode(texture: groundTexture)
+            ground.zPosition = -10
+            ground.position = CGPoint(x: (groundTexture.size().width/2.0 + (groundTexture.size().width*CGFloat(i))), y: groundTexture.size().height/2)
+            addChild(ground)
+            
+            let moveLeft = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5)
+            let moveReset = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0)
+            let moveLoop = SKAction.sequence([moveLeft, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            ground.run(moveForever)
+            
+        }
+    }
+    
+    func createRocks(){
+        let rockTexture = SKTexture(imageNamed: "rock")
+        
+        let topRock = SKSpriteNode(texture: rockTexture)
+        topRock.zPosition = -20
+        
+        let bottomRock = SKSpriteNode(texture: rockTexture)
+        bottomRock.zPosition = -20
+        
+        addChild(topRock)
+        addChild(bottomRock)
+        
+        let xPosition = frame.width + topRock.frame.width
+        let max = CGFloat(frame.height/3)
+        let yPosition = CGFloat.random(in: -50...max)
+        let rockDistance: CGFloat = 70
+        
+        topRock.position = CGPoint(x: xPosition, y: yPosition + topRock.size.height*1.5 + rockDistance)
+        bottomRock.position = CGPoint(x: xPosition, y: yPosition)
+        
+        let endPosition = frame.width+(topRock.frame.width*2)
+        let moveAction = SKAction.moveBy(x: -endPosition, y: 0, duration: 6)
+        let moveSequence = SKAction.sequence([moveAction, SKAction.removeFromParent()])
+
+        
+        topRock.run(moveSequence)
+        bottomRock.run(moveSequence)
+
+    }
+    
+    func startRocks(){
+        let create = SKAction.run { [unowned self] in
+            self.createRocks()
+        }
+        let wait = SKAction.wait(forDuration: 3)
+        let sequence = SKAction.sequence([create,wait])
+        let repeatForever = SKAction.repeatForever(sequence)
+        run(repeatForever)
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  
 }
